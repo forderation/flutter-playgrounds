19f11496d6c1857e137376155a22d7db5506aae5
forderation <kharisma.muzaki@gmail.com>
2020-10-06 00:01:36 +0700

    sliver flexible

--- shop_app/lib/screens/products_detail_screen.dart
+++ shop_app/lib/screens/products_detail_screen.dart
@@ -12,52 +12,59 @@ class ProductDetail extends StatelessWidget {
         Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  child: Image.network(loadedProduct.imageUrl),
-                  width: double.infinity,
-                ),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      width: double.infinity,
-                      color: Colors.black54,
-                      child: Text(
-                        'Price \$${loadedProduct.price}',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                loadedProduct.title,
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              background: Hero(
+                  tag: loadedProduct.id,
+                  child: Image.network(
+                    loadedProduct.imageUrl,
+                    fit: BoxFit.cover,
+                  )),
             ),
-            Divider(),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(horizontal: 15),
-              child: FittedBox(
-                alignment: Alignment.center,
-                fit: BoxFit.scaleDown,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                width: double.infinity,
+                color: Theme.of(context).primaryColor,
                 child: Text(
-                  loadedProduct.description,
-                  softWrap: true,
-                  style: TextStyle(fontSize: 16),
+                  'Price \$${loadedProduct.price}',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            )
-          ],
-        ),
+              Divider(),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                child: FittedBox(
+                  alignment: Alignment.center,
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    loadedProduct.description,
+                    softWrap: true,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 800,
+              )
+            ]),
+          )
+        ],
       ),
     );
   }
